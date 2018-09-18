7891b5c2aaac5249990bb88de055764cb9b812d4
sun <technesky@qq.com>
2018-09-18 17:14:13 +0800

    2018-09-18 eee

--- src/main/java/com/sky/easycode/controller/BookDownlistController.java
+++ src/main/java/com/sky/easycode/controller/BookDownlistController.java
@@ -14,6 +14,7 @@ import javax.annotation.Resource;
  */
 @Controller
 public class BookDownlistController {
+
     //protected Logger logger = Logger.getLogger(BookDownlistController.class);
 	//@Resource
 	//private BookDownlistService bookDownlistService;
